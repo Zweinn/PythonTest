e379500be3c1329782ea6e8c83c82172b0da3891
Zweinn <sven.techemn@gmx.de>
2025-09-29 14:06:42 +0200

    2025.09.29

---
+++ Daten.py
@@ -0,0 +1,7 @@
+x = "Hallo"  # STRING (str) > Zeichenkette, erlaubt kein Weiterrechnen
+print (x) 
+
+c=(50)
+print(c)# INTEGER (int) > Ganzzahl, für Berechnungen geeignet
+
+# Komma und Punkte werden in Python wie im Amerikanischen verwebdety daher der Punkt als trenner für Dezimalzahlen (50.5)
--- gametest/snake.py
+++ gametest/snake.py
@@ -8,4 +8,10 @@ screen = pygame.display.set_mode((WIN_SIZE, WIN_SIZE))
 
 run = True
 while run:
-    pygame.time.delay(20);
+    pygame.time.delay(20)
+
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            run = False
+
+            pygame.display.quit()
